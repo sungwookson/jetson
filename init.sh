0274571b456eaa4fd3d956aab4d4dce98dffcd04
Sungwook Son <sungwookson@gmail.com>
2021-08-10 16:14:33 +0900

    Changed appending fstab line

--- init.sh
+++ init.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
-#echo "jetson" | sudo -S reboot
-
-
 sudo systemctl disable nvzramconfig
 sudo fallocate -l 4G /mnt/4GB.swap
 sudo chmod 600 /mnt/4GB.swap
 sudo mkswap /mnt/4GB.swap
-sudo echo "/mnt/4GB.swap swap swap defaults 0 0" > /etc/fstab
+echo "/mnt/4GB.swap swap swap defaults 0 0" | sudo tee -a /etc/fstab
+
+sudo reboot
